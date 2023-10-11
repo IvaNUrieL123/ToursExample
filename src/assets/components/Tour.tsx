64a3052def813093962e1960412fd26f82e73328
IvaNUrieL123 <144205590+IvaNUrieL123@users.noreply.github.com>
2023-10-11 07:29:03 -0800

    Tour.tsx

--- src/assets/components/Tour.tsx
+++ src/assets/components/Tour.tsx
@@ -1,8 +1,44 @@
+import { useState } from 'react';
+import{Tour as TourType} from './types';
 
-const Tour = () => {
+
+type TourProps = {
+    tour:TourType;
+    
+}
+
+const Tour = ({tour}:TourProps) => {
+    const {image, name, price, info} = tour;
+    const [readMore, setReadMore] = useState(false);
+    const [isVisisble, setIsVisible] = useState(true);
+    
     return (
-        <div>Tour</div>
-    )
+        isVisisble && (
+            <article className="single-tour">
+                <img src={image} alt={name} className="img"/>
+                <span className="tour-price">${price}</span>
+                <div className="tour-info">
+                    <h5>{name}</h5>
+                    <p>
+                        {readMore ? info : info.substring(0,200) + '...'}
+                        <button onClick={() => setReadMore(!readMore)} className="info-btn">
+                            {readMore ? 'Show Less' : 'Read More' }
+                        </button>
+                    </p>
+                    <button 
+                        onClick={() => {
+                            setIsVisible(false);
+                        }}
+                        className="delete-btn btn-block btn" >
+                         not intersted 
+                    </button>
+                </div>    
+            </article>
+    
+         )
+         
+    );
+    
 };
 
-export default Tour
+export default Tour;
